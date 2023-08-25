038ad8f061c3375a4180c93f9d30de83292eaf26
nsdYuri <yurichichedom808@gmail.com>
2023-08-24 22:47:57 -0300

    cp1 - finalizado

--- menu.py
+++ menu.py
@@ -1,4 +1,6 @@
-users = {}
+from csvCP1 import load_users_from_csv, save_users_to_csv
+
+users = load_users_from_csv('cp1.csv')
 
 while True:
     print("\nUser Manager:")
@@ -8,27 +10,29 @@ while True:
     print("4. Search user by email")
     print("5. Exit")
     
-    choice = "5"
-
+    choice = input("Select option: ")
+    
     if choice == "1":
         name = input("Enter name: ")
         email = input("Enter email: ")
-        
+
         if email in users:
             print("User with this email already exists.")
         else:
             users[email] = name
             print(f"{name} added successfully!")
-
+            save_users_to_csv('cp1.csv', users)
+    
     elif choice == "2":
         email = input("Enter email to remove: ")
-        
+
         if email in users:
             del users[email]
             print(f"User with email {email} removed successfully!")
+            save_users_to_csv('cp1.csv', users)
         else:
             print("User not found.")
-
+            
     elif choice == "3":
         if not users:
             print("No users in the list.")
@@ -38,12 +42,12 @@ while True:
 
     elif choice == "4":
         email = input("Enter email to search: ")
-        
+
         if email in users:
             print(f"Name: {users[email]}, Email: {email}")
         else:
             print("User not found.")
-
+            
     elif choice == "5":
         print("Goodbye!")
-        break
+        break                    
